0fa410da2c8bc087282b21e8cfe9425082d9e927
Asya19 <asya.kisko@yandex.ru>
2024-02-13 22:05:48 +0300

    feat: add arrow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,13 @@ import { SliderInterface } from './gallery/types/slide.interface';
 
 export class AppComponent {
   slides: SliderInterface[] = [
-    { url: '\assets\images\png\img-1.jpg', title: 'crystal lake'},
-    { url: '\assets\images\png\img-2.jpg', title: 'crystal lake'},
-    { url: '\assets\images\png\img-3.jpg', title: 'crystal lake'},
-    { url: '\assets\images\png\img-4.jpg', title: 'crystal lake'},
-    { url: '\assets\images\png\img-5.jpg', title: 'crystal lake'},
-    { url: '\assets\images\png\img-6.jpg', title: 'crystal lake'},
-    { url: '\assets\images\png\img-7.jpg', title: 'crystal lake'},
-    { url: '\assets\images\png\img-8.jpg', title: 'crystal lake'},
+    { url: '/assets/images/png/img-1.jpg', title: 'crystal lake'},
+    { url: '/assets/images/png/img-2.jpg', title: 'crystal lake'},
+    { url: '/assets/images/png/img-3.jpg', title: 'crystal lake'},
+    { url: '/assets/images/png/img-4.jpg', title: 'crystal lake'},
+    { url: '/assets/images/png/img-5.jpg', title: 'crystal lake'},
+    { url: '/assets/images/png/img-6.jpg', title: 'crystal lake'},
+    { url: '/assets/images/png/img-7.jpg', title: 'crystal lake'},
+    { url: '/assets/images/png/img-8.jpg', title: 'crystal lake'},
   ];
 }
--- src/app/gallery/components/gallery/gallery.component.ts
+++ src/app/gallery/components/gallery/gallery.component.ts
@@ -8,5 +8,33 @@ import { SliderInterface } from '../../types/slide.interface';
   styleUrl: './gallery.component.css'
 })
 export class GalleryComponent {
+
+  /**
+   *
+   */
+  constructor() {}
+
   @Input() slides: SliderInterface[] = [];
+
+  currentIndex: number = 0;
+
+
+
+  goToNext(): void {
+    const IS_LAST_SLIDE: boolean = this.currentIndex === this.slides.length - 1;
+    const NEW_INDEX: number = IS_LAST_SLIDE ? 0 : this.currentIndex + 1;
+    this.currentIndex = NEW_INDEX;
+
+  }
+
+  goToPrevious(): void {
+    const IS_FIRST_SLIDE = this.currentIndex === 0;
+    const NEW_INDEX: number = IS_FIRST_SLIDE ? this.slides.length - 1 : this.currentIndex - 1;
+    this.currentIndex = NEW_INDEX;
+  }
+
+  getCurrentSlideUrl(): string {
+    return `url('${this.slides[this.currentIndex].url}')`;
+  }
+
 }
