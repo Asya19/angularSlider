a6c18b7783ec95eecbe44ca1e687e2d9bc96fdb5
Asya19 <asya.kisko@yandex.ru>
2024-02-14 21:32:57 +0300

    feat: add carousel

--- src/app/gallery/components/gallery/gallery.component.ts
+++ src/app/gallery/components/gallery/gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, ViewChild, ElementRef  } from '@angular/core';
 import { SliderInterface } from '../../types/slide.interface';
 // import { SliderInterface } from 'src/app/gallery/types/slide.interface';
 
@@ -7,16 +7,145 @@ import { SliderInterface } from '../../types/slide.interface';
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.css'
 })
-export class GalleryComponent {
 
-  /**
-   *
-   */
+// @Directive({
+//   standalone: true,
+//   selector: '[appHighlight]',
+// })
+
+
+export class GalleryComponent implements OnInit {
+
+  @ViewChild('ihCarouselInner') ihCarouselInner: ElementRef;
+
   constructor() {}
+
+  ngOnInit(): void {}
+  ngAfterViewInit() {
+    this.onResize();
+  }
+  onResize() {
+    setTimeout(() => {
+      this.toggleCarouselBtns();
+    }, 200);
+  }
+
+  // ngOnInit(): void {
+  //   throw new Error('Method not implemented.');
+  // }
+
+  // onResize() {
+  //   setTimeout(() => {
+  //     this.toggleCarouselBtns()
+  //   }, 200)
+  // }
 
   @Input() slides: SliderInterface[] = [];
 
   currentIndex: number = 0;
+
+
+  numberOfItems(num: number): number[] {
+    return new Array(num);
+  }
+
+  onCarouselArrowClick(direction: any) {
+    let carouselOffsetWidth = this.ihCarouselInner.nativeElement.offsetWidth;
+    let carouselItemWIdth = document.querySelector('.ih-carousel-item')?.clientWidth;
+
+    let carouselScrollWidth = this.ihCarouselInner.nativeElement.scrollWidth;
+    if (direction == 'left')
+      this.ihCarouselInner.nativeElement.scrollTo({
+        left:
+          this.ihCarouselInner.nativeElement.scrollLeft -
+          carouselOffsetWidth + carouselItemWIdth!,
+        behavior: 'smooth'
+      });
+    else
+      this.ihCarouselInner.nativeElement.scrollTo({
+        left:
+          this.ihCarouselInner.nativeElement.scrollLeft +
+          carouselOffsetWidth - carouselItemWIdth!,
+        behavior: 'smooth'
+      });
+
+    setTimeout(() => {
+      this.toggleCarouselBtns();
+    }, 200);
+  }
+
+  toggleCarouselBtns() {
+    let carouselScrollWidth = this.ihCarouselInner.nativeElement.scrollWidth;
+    let carouselOffsetWidth = this.ihCarouselInner.nativeElement.offsetWidth;
+    let scrollLeft = this.ihCarouselInner.nativeElement.scrollLeft;
+
+    let carouselPrevBtn: HTMLElement = document.getElementById('leftArrowCarousel')!;
+    let carouselNextBtn: HTMLElement = document.getElementById('rightArrowCarousel')!;
+
+    if (scrollLeft > 5) carouselPrevBtn.classList.remove('d-none');
+    else carouselPrevBtn.classList.add('d-none');
+
+    if (carouselOffsetWidth + scrollLeft + 25 >= carouselScrollWidth)
+      carouselNextBtn.classList.add('d-none');
+    else carouselNextBtn.classList.remove('d-none');
+  }
+
+  // onSwipe(swipe: any) {
+  //   //swipe-right = 4 & swipe-left = 2
+  //   console.log(swipe);
+  //   if (swipe.direction == 4) {
+  //     this.ihCarouselInner.nativeElement.scrollTo({
+  //       left: this.ihCarouselInner.nativeElement.scrollLeft - swipe.distance,
+  //       behavior: 'smooth'
+  //     });
+  //   }
+  //   if (swipe.direction == 2) {
+  //     this.ihCarouselInner.nativeElement.scrollTo({
+  //       left: this.ihCarouselInner.nativeElement.scrollLeft + swipe.distance,
+  //       behavior: 'smooth'
+  //     });
+  //   }
+  //   console.log(swipe.direction);
+  //   setTimeout(() => {
+  //     this.toggleCarouselBtns();
+  //   }, 200);
+  // }
+
+  // goToPreviousCarousel(): void {
+  //   console.log('btnLeft');
+  //   // const containerAllImg: any = document.getElementsByClassName('.container-all-img');
+  //   // containerAllImg.left = -25;
+  // }
+
+
+  // goToNextCarousel(): void {
+
+  //   // console.log('btnRight');
+
+  //   let btnRight = document.getElementById('rightArrowCarousel');
+
+  //   const containerAllImg: any = document.getElementById('container-all-img');
+  //   // containerAllImg.style.left = 0;
+  //   btnRight?.addEventListener('click', function() {
+  //       console.log('btnRight');
+
+
+  //       let p = 0;
+  //       containerAllImg.style.left += p - 25 + '%';
+  //     })
+
+  //   // const containerAllImg: any = document.getElementsByClassName('.container-all-img');
+  //   // containerAllImg.nativeElement.style.left = '-25';
+  //   // containerAllImg.nativeElement.scrollTo({ left: (-25)});
+
+
+  //   // let btnRight = document.getElementById('btnRight');
+
+  //   // btnRight?.addEventListener('click', function() {
+  //   //   console.log('btnRight');
+  //   // })
+
+  // }
 
   goToSlide(slideIndex: number): void {
     this.currentIndex = slideIndex;
